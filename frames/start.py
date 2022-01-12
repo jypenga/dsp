eb9b5cf17486751fe9fd5cbb82771f39b028fec8
jypenga <jorrit.ypenga1@gmail.com>
2022-01-12 14:14:16 +0100

    style update

--- frames/start.py
+++ frames/start.py
@@ -13,8 +13,7 @@ cstm.RegisterHeader(),
 html.Div([
     html.P('Ben jij mantelzorger en heb je hulp nodig bij het verzorgen van jouw patient(en)?'),
     html.P('Met MoniZorg kun je gemakkelijk jouw patienten monitoren en de juiste zorg bieden die jouw patient op dat moment nodig heeft.'),
-    html.Button(dcc.Link('REGISTREREN', href='/registreren'), 
-    id='app-free-btn')], 
+    dcc.Link('REGISTREREN', href='/registreren', id='app-free-btn')],
 className='app-body'),
 cstm.RegisterFooter()
 ]
--- templates/main.py
+++ templates/main.py
@@ -3,9 +3,11 @@ import dash
 from dash import dcc
 from dash import html
 
+
 class Custom():
     def __init__(self):
         self.app = dash.Dash(__name__)
+
 
     def RegisterHeader(self):
         content = html.Div([
@@ -20,25 +22,29 @@ class Custom():
                 className='register-header')
         return content
 
+
     def RegisterFooter(self):
         content = html.Div([], 
                     className='register-footer')
         return content
 
+
     def RegisterTable(self):
-        content = html.Table([html.Tr([html.Td(html.Img(src=self.app.get_asset_url('icon-user.svg'))), html.Td('Naam'), html.Td(dcc.Input(id='input-user', type='text'))]),
+        content = html.Table(html.Tbody([html.Tr([html.Td(html.Img(src=self.app.get_asset_url('icon-user.svg'))), html.Td('Naam'), html.Td(dcc.Input(id='input-user', type='text'))]),
                 html.Tr([html.Td(html.Img(src=self.app.get_asset_url('icon-age.svg'))), html.Td('Leeftijd'), html.Td(dcc.Input(id='input-age', type='number'))]),
                 html.Tr([html.Td(html.Img(src=self.app.get_asset_url('icon-sex.svg'))), html.Td('Geslacht'), html.Td(dcc.Dropdown(id='input-sex', options=[{'label':'man', 'value':'m'}, {'label':'vrouw', 'value':'v'}, {'label':'anders', 'value':'a'}], placeholder='', clearable=False))]),
                 html.Tr([html.Td(html.Img(src=self.app.get_asset_url('icon-email.svg'))), html.Td('Email'), html.Td(dcc.Input(id='input-email', type='email'))]),
                 html.Tr([html.Td(html.Img(src=self.app.get_asset_url('icon-phone.svg'))), html.Td('Tel.'), html.Td(dcc.Input(id='input-phone', type='tel'))]),
                 html.Tr(html.Td(dcc.Checklist(id='input-privacy', options=[{'label':' Ik ben het eens met de blablabla', 'value':'checked'}]), colSpan=3))
-                ], id='app-register-table')
+                ]), id='app-register-table')
         return content
 
+
     def LoginTable(self):
-        content = [html.Table([html.Tr(html.Td('Emailadres')), html.Tr(html.Td(dcc.Input(id='login-input-email', type='email')))], className='login-table'),
-                html.Table([html.Tr(html.Td('Wachtwoord')), html.Tr(html.Td(dcc.Input(id='login-input-password', type='password')))], className='login-table')]
+        content = [html.Table(html.Tbody([html.Tr(html.Td('Emailadres')), html.Tr(html.Td(dcc.Input(id='login-input-email', type='email')))]), className='login-table'),
+                html.Table(html.Tbody([html.Tr(html.Td('Wachtwoord')), html.Tr(html.Td(dcc.Input(id='login-input-password', type='password')))]), className='login-table')]
         return content
+
 
     def AppHeader(self, color=None, middle=None, top_left=None, top_right=None):
         content = html.Div([
@@ -58,6 +64,7 @@ class Custom():
                         className='app-header-text-middle')
                 ], className='app-header')
         return content
+
 
     def AppFooter(self, color=None, buttons=True):
         content = None
